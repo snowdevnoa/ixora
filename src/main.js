bffa62662cbefe9fc8675484d9109e600e167e59
Noa <snowdevnoa@gmail.com>
2024-11-22 11:41:37 -0600

    Reorganize and insert folder structure

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
-import router from './router'
+import router from './router/router'
 import App from './App.vue'
 
 const app = createApp(App)
--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 import { createMemoryHistory, createRouter } from 'vue-router';
 
-import HelloWorldView from './components/HelloWorld.vue';
-import HomeView from './components/Home.vue';
+import HelloWorldView from '../views/HelloWorld.vue';
+import HomeView from '../views/Home.vue';
 
 const routes = [
 	{ path: '/', component: HomeView },
